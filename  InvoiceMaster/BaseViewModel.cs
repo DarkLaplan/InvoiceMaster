8b556286a74ce366b6c065d70b533bd08b0e2cb5
Lansky Michal <Lansky.michal3@gmail.com>
2019-03-06 22:26:29 +0100

    Added relay command and baseviewmodel classes. Created new behavioral for WebBrowser control.

---
+++  InvoiceMaster/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace InvoiceMaster
+{
+    /// <summary>
+    /// Basic abstract class which should implements each viewmodel class.
+    /// </summary>
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Event for updating UI after property is changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Method is called to init changes in UI for concreate property.
+        /// </summary>
+        /// <param name="propertyName">Name of property.</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++  InvoiceMaster/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace InvoiceMaster
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Predicate<object> _canExecute;
+        readonly Action<object> _execute;
+
+        public RelayCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute,
+            Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new NullReferenceException("execute");
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null ? true : _canExecute(parameter);
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
+        }
+    }
+}
